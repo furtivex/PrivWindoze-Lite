80592de0dd220e3cfc51f4e6eac953762cc5074a
d1savow3d <35790653+thisisu@users.noreply.github.com>
2024-11-17 11:09:28 -0600

    lite

--- PrivWindoze.bat
+++ PrivWindoze.bat
@@ -1,4 +1,4 @@
-:: PrivWindoze
+:: PrivWindoze Lite
 :: Created by Furtivex
 @echo OFF && color 17
 title PrivWindoze Lite by Furtivex - Version 2.6.9
@@ -607,8 +607,8 @@ FOR %%g in (
 :DoLog
 
 Echo(~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>"%TEMP%\pwindoze.txt"
-Echo(PrivWindoze by Furtivex>>"%TEMP%\pwindoze.txt"
-Echo(Version: 2.6.8 ^(11.17.2024^)>>"%TEMP%\pwindoze.txt"
+Echo(PrivWindoze Lite by Furtivex>>"%TEMP%\pwindoze.txt"
+Echo(Version: 2.6.9 ^(11.17.2024^)>>"%TEMP%\pwindoze.txt"
 Echo(Operating System: %OS% %ARCH%>>"%TEMP%\pwindoze.txt"
 Echo(Ran by "%username%" ^("%COMPUTERNAME%"^) ^(%USERSTATUS%^) on %StartDate% at %StartTime%>>"%TEMP%\pwindoze.txt"
 Echo(~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>>"%TEMP%\pwindoze.txt"
